e20f6dab52ba568cca3aec4e5574479995936b65
omgursocute <thewminers1014@gmail.co>
2020-03-06 21:44:07 +0700

    .

--- assets/js/register.js
+++ assets/js/register.js
@@ -3,12 +3,30 @@ function FormWarning(str) {
 	warn.innerHTML = str;
 }
 
+async function move(s) {
+	if($('div.active').index() == 2) quit();
+	$('#newuser-carousel').carousel(s);
+	await sleep(750);
+	if($('div.active').index() == 2) init();
+}
+
+function show_confirm() {
+	$('#confirm-form').modal('toggle');
+}
+
+function dismiss_modal() {
+	if($('div.active').index() == 2) quit();
+	// confirm
+	$('#confirm-form').modal('toggle');
+	$('#newuserform').modal('toggle');
+}
+
+var name = null;
+var phonenumber = null;
+var city = null;
+var country = null;
+
 function newuser() {
-	// var name = document.getElementById('fullname');
-	// var phonenumber = document.getElementById('phone-number');
-	// var validname = name.validity.valid;
-	// var validphonenumber = phonenumber.validity.valid;
-	
 	// if(!validname && !validphonenumber)
 	// 	FormWarning('Tên và SĐT đều trống!');
 	// else if(!validname)
@@ -24,20 +42,16 @@ function newuser() {
 	move('next'); // from register.js
 }
 
-async function move(s) {
-	if($('div.active').index() == 2) quit();
-	$('#newuser-carousel').carousel(s);
-	await sleep(1000);
-	if($('div.active').index() == 2) {
-		console.log($('div.active').index());
-		init();
-	}
-}
-
 function validateCheckbox() {
 	var a = [];
 	$("input:checkbox[name=checkbox-list]:checked").each(function() {
     	a.push($(this).val());
 	});
+	if(a.length < 3) {
+		alert('not enough');
+	}
+	else {
+		move('next');
+	}
 	console.log(a);
 }
