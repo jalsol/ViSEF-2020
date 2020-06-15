da28555fd25bcf682aad65ca929bd5bac6bde738
jalsol <jalsol@pm.me>
2020-06-15 12:45:56 +0700

    .

--- backend/test2.sh
+++ backend/test2.sh
@@ -1,4 +1,4 @@
-# cd ../ML/face-recognition; pwd; konsole -e "echo 'hi'"
+cd ../ML/face-recognition; pwd; /usr/bin/python3 -m inference.video_classifier
 # python3 create_local_process.py
 # /usr/bin/python3 -m inference.video_classifier
-DISPLAY:=0; xterm -e "vlc" &
+# DISPLAY:=0; xterm -e "vlc" &
